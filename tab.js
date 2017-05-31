b80b620f0c58ab3326031d63ca6e6ef8ffcd43db
manoj <manoj.g@reactore.com>
2017-05-31 13:09:05 +0530

    feat:initial electron app commit

---
+++ tab.js
@@ -0,0 +1,15 @@
+angular.module('tabModule', ['ui.bootstrap']);
+var TabsDemoCtrl = function ($scope) {
+    $scope.data = {
+        static: true
+    }
+    $scope.panes = [{
+            title: "Dynamic Title 1",
+            content: "Dynamic content 1"
+        },
+        {
+            title: "Dynamic Title 2",
+            content: "Dynamic content 2"
+        }
+    ];
+};
---
+++ view.js
@@ -0,0 +1,8 @@
+/*Building UI's */
+let $ = require('jquery');
+let count = 0;
+$('#click-counter').text(count.toString());
+$('#countbtn').on('click', () => {
+    count++;
+    $('#click-counter').text(count);
+})
